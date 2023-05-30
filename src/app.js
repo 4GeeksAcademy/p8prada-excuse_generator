f47de6821c4d7a25d3d41dd345483cb911266c72
p8prada <131833040+p8prada@users.noreply.github.com>
2023-05-30 15:23:51 +0000

    v1 random excuse generator + style

--- src/app.js
+++ src/app.js
@@ -2,7 +2,50 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello JULIAN");
+window.onload = () => {
+  let randomChoise = () => Math.floor(Math.random() * 6);
+
+  const generateExcuse = () => {
+    let who = [
+      "My dog ",
+      "My cat ",
+      "My Grandma ",
+      "My uncle ",
+      "My girlfriend ",
+      "My horse "
+    ];
+    let action = [
+      "ate ",
+      "peed ",
+      "jumped over ",
+      "took ",
+      "squeezed ",
+      "sat over "
+    ];
+    let what = [
+      "a blowfish ",
+      "the keys ",
+      "the car ",
+      "the rubber duck ",
+      "the cactus ",
+      "a bottle "
+    ];
+    let when = [
+      "before the class.",
+      "right on time.",
+      "when I arrived.",
+      "during my lunch.",
+      "while I was praying.",
+      "during the night."
+    ];
+
+    return (
+      who[randomChoise()] +
+      action[randomChoise()] +
+      what[randomChoise()] +
+      when[randomChoise()]
+    );
+  };
+
+  document.querySelector("#excuse").innerHTML = generateExcuse();
 };
